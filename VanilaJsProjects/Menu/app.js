84e257acc0a2c660bfc62c14b35a71a7f59e911b
sakshi1feb <sakshi1feb@gmail.com>
2023-06-25 09:45:46 +0530

    made the menu project as per instructions

--- VanilaJsProjects/Menu/app.js
+++ VanilaJsProjects/Menu/app.js
@@ -75,11 +75,46 @@ const menu = [
 ];
 
 const sectionCenter=document.querySelector(".section-center");
+const filterBtns=document.querySelectorAll(".filter-btn");
 
+
+//load items
 window.addEventListener('DOMContentLoaded',function(){
-    let displayMenu=menu.map(function(item){
-        // console.log(item);
-        return '<h1>$(item.title</h1>';
-    });
-    console.log(displayMenu);
-})
+      displayMenuItems(menu);
+});
+// filter items
+filterBtns.forEach(function(btn){
+  btn.addEventListener("click",function(e){
+      const category = e.currentTarget.dataset.id;
+      const menuCategory=menu.filter(function(filterItem){
+        if(filterItem.category===category)
+            return filterItem;
+      });
+
+      if(category === "all")
+        return displayMenuItems(menu);
+      else
+        return displayMenuItems(menuCategory);
+  });
+});
+
+
+
+
+function displayMenuItems(menuItems){
+  let displayMenu=menuItems.map(function(item){
+    // console.log(item);
+    return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt=${item.title}>
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+      <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">${item.desc}</p>
+    </div>
+  </article>`;
+});
+displayMenu=displayMenu.join("")
+sectionCenter.innerHTML=displayMenu;
+}
